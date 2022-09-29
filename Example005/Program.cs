5623d3a5e232a3161d3f99eb815f735f6235cd35
Svetlana <sveta.yakovenko97@mail.ru>
2022-09-29 19:28:31 +0300

    add Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+   Console.WriteLine("Привет,дорогая!");
+}
+else
+{
+    Console.Write("Hello");
+    Console.WriteLine(username);
+}
